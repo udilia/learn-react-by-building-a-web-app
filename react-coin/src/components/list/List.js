ab5f35c12bc9e4eccf0a9aa6c406b05679ff25a3
Alex Pattison <alexlpattison@gmail.com>
2018-04-17 15:43:02 -0700

    Move table logic into its own component

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Loading from '../common/Loading';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
-import './Table.css';
+import Table from './Table.js';
 
 class List extends React.Component {
   constructor() {
@@ -56,39 +56,7 @@ class List extends React.Component {
     }
 
     else {
-      return <div>
-        <table className='Table'>
-          <thead className='Table-head'>
-            <tr>
-              <th>Cryptocurrency</th>
-              <th>Price</th>
-              <th>Market Cap</th>
-              <th>24H Change</th>
-            </tr>
-          </thead>
-          <tbody className='Table-body'>
-            {currencies.map((currency) => (
-              <tr key={currency.id}>
-                <td>
-                  <span className='Table-rank'>{currency.rank}</span>
-                  {currency.name}
-                </td>
-                <td>
-                  <span className='Table-dollar'>$</span>
-                  {currency.price}
-                </td>
-                <td>
-                  <span className='Table-dollar'>$</span>
-                  {currency.marketCap}
-                </td>
-                <td>
-                  {this.renderChangePercent(currency.percentChange24h)}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      return <Table currencies={currencies} renderChangePercent={this.renderChangePercent}/>
     }
   }
 }
---
+++ react-coin/src/components/list/Table.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Table.css';
+
+const Table = ({ currencies, renderChangePercent }) => {
+  return (
+    <div>
+      <table className='Table'>
+        <thead className='Table-head'>
+          <tr>
+            <th>Cryptocurrency</th>
+            <th>Price</th>
+            <th>Market Cap</th>
+            <th>24H Change</th>
+          </tr>
+        </thead>
+        <tbody className='Table-body'>
+          {currencies.map((currency) => (
+            <tr key={currency.id}>
+              <td>
+                <span className='Table-rank'>{currency.rank}</span>
+                {currency.name}
+              </td>
+              <td>
+                <span className='Table-dollar'>$</span>
+                {currency.price}
+              </td>
+              <td>
+                <span className='Table-dollar'>$</span>
+                {currency.marketCap}
+              </td>
+              <td>
+                {renderChangePercent(currency.percentChange24h)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table;
