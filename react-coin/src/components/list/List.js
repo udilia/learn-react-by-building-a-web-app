3636f59c3ccb37143219cdf417ef50e75217174c
Alex Pattison <alexlpattison@gmail.com>
2018-04-16 22:28:35 -0700

    Add UI logic for the List component
    Currently displaying only the first 20 cryptos by rank. This is
    hardcoded at the moment.

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { handleResponse } from '../../helpers';
 import { API_URL } from '../../config';
+import './Table.css'
 
 class List extends React.Component {
   constructor() {
@@ -32,11 +33,55 @@ class List extends React.Component {
       });
   }
 
+  renderChangePercent(percent) {
+    if (percent > 0) {
+      return <span className='percent-raised'>{percent}% &uarr;</span>
+    } else if (percent < 0) {
+      return <span className='percent-fallen'>{percent}% &darr;</span>
+    } else {
+      return <span>{percent}%</span>
+    }
+  }
+
   render() {
     if (this.state.loading === true) {
       return <div>Loading...</div>
     } else {
-      return <div>text</div>
+      return <div>
+        <table className='Table'>
+          <thead className='Table-head'>
+            <tr>
+              <th>Cryptocurrency</th>
+              <th>Price</th>
+              <th>Market Cap</th>
+              <th>24H Change</th>
+            </tr>
+          </thead>
+          <tbody className='Table-body'>
+            {this.state.currencies.map((currency) => (
+              <tr key={currency.id}>
+                <td>
+                  <span className='Table-rank'>{currency.rank}</span>
+                  {currency.name}
+                </td>
+                <td>
+                  <span className='Table-dollar'>$</span>
+                  {currency.price}
+                </td>
+                <td>
+                  <span className='Table-dollar'>$</span>
+                  {currency.marketCap}
+                </td>
+                <td>
+                  {this.renderChangePercent(currency.percentChange24h)}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+
+      </div>
     }
   }
 }
