2c4aa7f9e45d82c8f7cf5f4cadd7fcb0af1e7974
Alex Pattison <alexlpattison@gmail.com>
2018-04-16 22:18:32 -0700

    Store API endpoint in config.js

--- react-coin/src/components/list/List.js
+++ react-coin/src/components/list/List.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { handleResponse } from '../../helpers';
+import { API_URL } from '../../config';
 
 class List extends React.Component {
   constructor() {
@@ -15,7 +16,7 @@ class List extends React.Component {
   componentDidMount() {
     this.setState({ loading: true })
 
-    fetch('https://api.udilia.com/coins/v1/cryptocurrencies?page=1&perPage=20')
+    fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
       .then(handleResponse)
       .then((data) => {
         this.setState({
