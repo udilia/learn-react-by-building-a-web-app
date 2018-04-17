29799961b0e0de8f0c737186d49f263232b280b2
Alex Pattison <alexlpattison@gmail.com>
2018-04-16 20:53:19 -0700

    Add minimal list component
    Fetches data and sets state. UI will be handled in next commit.

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Header from './components/common/Header';
+import List from './components/list/List';
 import './index.css';
 
 const App = () => {
-  const title = 'React Coin';
   return <div>
     <Header />
-    <h1>{title}</h1>
+    <List />
   </div>
 };
 
