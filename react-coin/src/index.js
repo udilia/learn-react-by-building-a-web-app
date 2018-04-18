bbe241a8a8da85948be20fc91d83a4f88f774c2d
Alex Pattison <alexlpattison@gmail.com>
2018-04-18 11:43:46 -0700

    Add routing and NotFound component

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
+import NotFound from './components/notFound/NotFound';
 import './index.css';
 
 const App = () => {
-  return <div>
-    <Header />
-    <List />
-  </div>
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+
+        <Switch>
+          <Route path="/" component={List} exact />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 };
 
 render(<App/>, document.getElementById('root'));
