36297e863c04cfc4276f7e6edbe9d36acbee336c
Alex Pattison <alexlpattison@gmail.com>
2018-04-23 12:38:34 -0700

    Make header a link to homepage

--- react-coin/src/components/common/Header.js
+++ react-coin/src/components/common/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import './Header.css';
 
 const Header = () => {
 	return (
 		<div className='Header'>
-			<img src={logo} alt='logo' className='Header-logo' />
+			<Link to="/">
+				<img src={logo} alt='logo' className='Header-logo' />
+			</Link>
 		</div>
 	)
 }
